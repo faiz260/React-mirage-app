63171e500cfed6e72c1e9e123bf54a8a66cec227
faiz <razafaiz815@gmail.com>
2020-11-10 00:22:58 +0500

    mirage server added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [books, setBooks] = useState([{}]);
+
+  useEffect(() => {
+    setInterval(() => {
+      fetch("/api/books")
+        .then((res) => res.json())
+        .then((data) => {
+          setBooks(data);
+        });
+    }, 2000);
+  }, []);
+
+  const addBook = () => {
+    console.log("Book Added");
+    const title = prompt("Enter Book Title");
+    const author = prompt("Enter Book Author");
+    
+    if(!title || !author)
+      return false;
+
+    fetch("/api/add", {
+      method: "POST",
+      body: JSON.stringify({ title, author }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log("data", data));
+  };
+
+  if(!books.length)
+    return <h2>Loading...</h2>
+ 
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Available Books</h1>
+      <table>
+        {books.map((booksObj, ind) => {
+          return (
+            <tr key={ind}>
+              <td >{booksObj.title}</td>
+              <td>{booksObj.author}</td>
+            </tr>
+          );
+        })}
+      </table>
+      <button onClick={addBook}>Add Book</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {makeServer} from './Server/server';
+
+makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
